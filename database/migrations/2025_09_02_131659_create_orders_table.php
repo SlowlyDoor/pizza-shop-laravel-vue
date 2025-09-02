fd62827e55ee178ee3bedeffbd46c3162916f9aa
SlowlyDoor <internalhell@yandex.ru>
2025-09-02 16:21:33 +0300

    Добавление таблиц

---
+++ database/migrations/2025_09_02_131659_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('address');
+            $table->string('phone');
+            $table->text('comment')->nullable();
+            $table->json('items_json');
+            $table->decimal('total_price', 10, 2);
+            $table->string('status')->default('new');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
